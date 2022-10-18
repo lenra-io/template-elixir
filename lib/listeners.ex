45c9d34f2a8e9e382e51798d865d53f06fbb295d
Louis G <louis@lenra.me>
2022-10-18 10:38:43 -0300

    WIP

--- lib/listeners.ex
+++ lib/listeners.ex
@@ -1,4 +1,6 @@
 defmodule Listeners do
-  def run(conn, body_params) do
+  def run(_conn, _body_params) do
+    # Find a way to call the correct listener according to the action property.
+    # If possible, avoid to "register" the listeners manually ?
   end
 end
